cc18b566071b0d17a17c314c969d8588dfea22e5
avidrucker <avidrucker@gmail.com>
2025-09-14 14:49:03 -1000

    refactor comment out console log

--- src/hooks/useMapLoading.js
+++ src/hooks/useMapLoading.js
@@ -166,13 +166,13 @@ export function useMapLoading({
             }
             
             // Reset camera + fit
-            console.log("useMapLoading: Internally resetting camera due to URL change");
+            // console.log("useMapLoading: Internally resetting camera due to URL change");
             // dispatchAppState({ type: ACTION_TYPES.SET_ZOOM_INTERNAL, payload: { zoom: 1 } });
             // dispatchAppState({ type: ACTION_TYPES.SET_CAMERA_POSITION_INTERNAL, payload: { position: { x: 0, y: 0 } } });
             setTimeout(() => {
               dispatchAppState({ type: ACTION_TYPES.SET_SHOULD_FIT, payload: { shouldFit: true } });
             }, 10);  
-                      
+
             // Clear selections and undo state (loading clears undo)
             clearCytoscapeSelections();
             dispatchAppState({ type: ACTION_TYPES.CLEAR_ALL_SELECTIONS });
